12f96ece307536903a9e83ac151eb9fc3d93aefb
VladislavAduenko <adyenko.job@gmail.com>
2024-12-04 23:23:08 +0200

    HW-46: Add HW solution #46

---
+++ src/main/java/telran/numbers/ThreadsPoolGroupSum.java
@@ -0,0 +1,25 @@
+package telran.numbers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+public class ThreadsPoolGroupSum extends ThreadsGroupSum {
+    private final ExecutorService executor =  Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ThreadsPoolGroupSum(int[][] groups) {
+        super(groups);
+       
+    }
+
+    @Override
+    protected void startTasks(FutureTask<Long>[] tasks) {
+        for (int i = 0; i < tasks.length; i++) {
+            tasks[i] = new FutureTask<>(new OneGroupSum(groups[i]));
+            executor.execute(tasks[i]);
+        }
+
+        executor.shutdown();
+    }
+
+}
